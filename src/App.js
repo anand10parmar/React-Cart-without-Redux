3326d8ec1233a247f5ab6dbce676a280e7b45120
anand10parmar <anand10parmar@gmail.com>
2020-11-08 15:17:32 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -21,16 +21,73 @@ class App extends Component {
             },
             products
         }
+        this.addOne = this.addOne.bind(this);
+        this.removeOne = this.removeOne.bind(this);
+        this.addToCart = this.addToCart.bind(this);
     }
 
+    addOne(product) {
+        const elementsIndex = this.state.cart.items.findIndex(element => element.id === product.id)
+        let cartItems = [...this.state.cart.items]
+        cartItems[elementsIndex] = { ...cartItems[elementsIndex], quantity: cartItems[elementsIndex].quantity + 1 }
+
+        const elementsIndexPro = this.state.products.findIndex(element => element.id === product.id)
+        let productItems = [...this.state.products]
+        productItems[elementsIndexPro] = { ...productItems[elementsIndexPro], cartQuantity: productItems[elementsIndexPro].cartQuantity + 1 }
+
+        this.setState({
+            cart: {
+                items: [...cartItems]
+            },
+            products: [...productItems]
+        })
+    }
+
+    removeOne(product) {
+        const elementsIndex = this.state.cart.items.findIndex(element => element.id === product.id)
+        let cartItems = [...this.state.cart.items]
+        cartItems[elementsIndex] = { ...cartItems[elementsIndex], quantity: cartItems[elementsIndex].quantity - 1 }
+
+        const elementsIndexPro = this.state.products.findIndex(element => element.id === product.id)
+        let productItems = [...this.state.products]
+        productItems[elementsIndexPro] = { ...productItems[elementsIndexPro], cartQuantity: productItems[elementsIndexPro].cartQuantity - 1 }
+
+        //condition when quantity is one
+        cartItems = cartItems.filter((el)=> { return (product.cartQuantity === 1&& product.id === el.id)?el.quantity > 1 :cartItems });
+
+        this.setState({
+            cart: {
+                items: [...cartItems]
+            },
+            products: [...productItems]
+        })
+    }
+
+    addToCart(product) {
+        const cartItems = [{ id: product.id, item: product.name, quantity: product.cartQuantity + 1 }]
+        product.cartQuantity++
+
+        this.setState({
+            cart: {
+                items: [...this.state.cart.items, ...cartItems]
+            },
+            products: [...this.state.products]
+        })
+
+    }
 
     render() {
         return (
             <div>
                 <h8k-navbar header={title}></h8k-navbar>
                 <div className="layout-row shop-component">
-                    <ProductList products={this.state.products}/>
-                    <Cart cart={this.state.cart}/>
+                    <ProductList
+                        cart={this.state.cart}
+                        products={this.state.products}
+                        addOne={this.addOne}
+                        removeOne={this.removeOne}
+                        addToCart={this.addToCart} />
+                    <Cart cart={this.state.cart} />
                 </div>
             </div>
         );
--- src/components/product-list/index.js
+++ src/components/product-list/index.js
@@ -1,8 +1,8 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./index.css";
 
 export default class ProductList extends Component {
-    constructor() {
+    constructor(props) {
         super();
     }
 
@@ -12,36 +12,40 @@ export default class ProductList extends Component {
                 {this.props.products.map((product, i) => {
                     return (
                         <section className="w-30"
-                                 data-testid={'product-item-' + i}
-                                 key={product.id}>
+                            data-testid={'product-item-' + i}
+                            key={product.id}>
                             <div className="card ma-16">
                                 <img alt="Your Cart" src={product.image}
-                                     className="d-inline-block align-top product-image"/>
+                                    className="d-inline-block align-top product-image" />
                                 <div className="card-text pa-4">
                                     <h5 className="ma-0 text-center">{product.name}</h5>
                                     <p className="ma-0 mt-8 text-center">${product.price}</p>
                                 </div>
                                 <div className="card-actions justify-content-center pa-4">
-
-                                    <button className="x-small outlined" data-testid="btn-item-add">
+                                   {product.cartQuantity>0? '': <button
+                                        className="x-small outlined"
+                                        data-testid="btn-item-add"
+                                        onClick={() => this.props.addToCart(product)}                                    >
                                         Add To Cart
-                                    </button>
-
-                                    <div className="layout-row justify-content-between align-items-center">
+                                   </button> }
+                                    {product.cartQuantity===0?'':<div className="layout-row justify-content-between align-items-center">
                                         <button className="x-small icon-only outlined"
-                                                data-testid="btn-quantity-subtract">
+                                            onClick={() => this.props.removeOne(product)}
+                                            data-testid="btn-quantity-subtract">
                                             <i className="material-icons">remove</i>
                                         </button>
 
                                         <input type="number"
-                                               disabled
-                                               className="cart-quantity" data-testid="cart-quantity"/>
+                                            value={product.cartQuantity}
+                                            disabled
+                                            className="cart-quantity" data-testid="cart-quantity" />
 
                                         <button className="x-small icon-only outlined"
-                                                data-testid="btn-quantity-add">
+                                            onClick={() => this.props.addOne(product)}
+                                            data-testid="btn-quantity-add">
                                             <i className="material-icons">add</i>
                                         </button>
-                                    </div>
+                                    </div>}
 
                                 </div>
                             </div>
